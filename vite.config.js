20a384b2ffeb4909983d9e802cf684b158918be1
Robert Lee <rlee@crewmp.com>
2023-04-03 13:07:14 -0700

    Update config files

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,26 @@ import create_config from '@kucrut/vite-for-wp';
 
 export default create_config(
     {
-        app: './resources/assets/js/app.js',
-        editor: './resources/assets/js/editor.js'
+      app: './resources/assets/js/app.js',
+      editor: './resources/assets/js/editor.js'
     },
     'dist',
     {
-        resolve: {
-            alias: {
-            '@': path.resolve(__dirname, 'resources'),
-            }
+      resolve: {
+        alias: {
+        '@': path.resolve(__dirname, 'resources'),
+        '~': path.resolve(__dirname)
         }
+      },
+      optimizeDeps: {
+        include: [
+            'tailwind.config.js',
+        ]
+      },
+      build: {
+        commonjsOptions: {
+          include: ['tailwind.config.js', 'node_modules/**'],
+        },
+      },
     }
 );
