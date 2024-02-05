aa6cbb47e8fb4b5f6115ce7d48156b8d1516ec88
Robert Lee <rlee@crewmp.com>
2024-02-05 13:34:23 -0800

    Bump dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,36 @@
-/** @type {import('vite').UserConfig} */
-var path = require('path');
+import { resolve } from 'node:path'
+import { v4wp } from '@kucrut/vite-for-wp'
 
-import create_config from '@kucrut/vite-for-wp';
-
-export default create_config(
-  {
-    app: './resources/assets/js/app.js',
-    editor: './resources/assets/js/editor.js'
-  },
-  'dist',
-  {
-    plugins: [],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'resources'),
-        '~': path.resolve(__dirname),
-        'tailwind.config.js': path.resolve(__dirname, 'tailwind.config.js'),
-      }
-    },
-    optimizeDeps: {
-      include: [
-        'tailwind.config.js',
-      ]
-    },
-    build: {
-      commonjsOptions: {
-        include: ['./tailwind.config.js', 'node_modules/**'],
+export default {
+  plugins: [
+    v4wp({
+      input: {
+        app: './resources/assets/js/app.js',
+        editor: './resources/assets/js/editor.js'
       },
+      outDir: 'dist'
+    })
+  ],
+  server: {
+    watch: {
+      ignored: ["**/vendor/**"],
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'resources'),
+      '~': resolve(__dirname),
+      'tailwind.config.js': resolve(__dirname, 'tailwind.config.js'),
+    }
+  },
+  optimizeDeps: {
+    include: [
+      'tailwind.config.js',
+    ]
+  },
+  build: {
+    commonjsOptions: {
+      include: ['./tailwind.config.js', /node_modules/],
     },
   }
-);
+}
